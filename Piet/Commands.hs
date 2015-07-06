ebf9d719510f58f1f64dadca7e190b8c2f010315
lesguillemets <lesguillemets@users.noreply.github.com>
2015-07-06 14:08:15 +0900

    ignore non-executable commands

--- Piet/Commands.hs
+++ Piet/Commands.hs
@@ -12,7 +12,9 @@ import Helpers
 updMemWith :: ([Integer] -> [Integer]) -> Machine -> Machine
 updMemWith f m@(Machine {..}) = m {mem = f mem}
 updMemWithOp :: (Integer -> Integer -> Integer) -> Machine -> Machine
-updMemWithOp op = updMemWith $ \ (x:y:xs) -> (y `op` x):xs
+updMemWithOp _ m@(Machine {mem=[]}) = m
+updMemWithOp _ m@(Machine {mem=[_]}) = m
+updMemWithOp op m = flip updMemWith m $ \ (x:y:xs) -> (y `op` x):xs
 
 data Command = Skip
              | Push
@@ -46,6 +48,7 @@ apply :: Command -> Integer -> Machine -> IO Machine
 apply Skip _ m = return m
 
 apply Push n m = return . updMemWith (n:) $ m
+apply Pop _ m@(Machine{mem=[], ..}) = return m
 apply Pop _  m = return . updMemWith tail $ m
 
 apply Add _ m = return . updMemWithOp (+) $ m
@@ -55,25 +58,31 @@ apply Multiply _ m = return . updMemWithOp (*) $ m
 apply Divide _ m = return . updMemWithOp div $ m
 apply Mod _ m = return . updMemWithOp mod $ m
 
+apply Not _ m@(Machine{mem=[], ..}) = return m
 apply Not _ m = return . updMemWith
     (\(x:r) -> if x == 0 then 1:r else 0:r) $ m
 apply Greater _ m = return . updMemWithOp
     (((toInteger . fromEnum) . ) .  (>)) $ m
 
+apply Pointer _ m@(Machine{mem=[], ..}) = return m
 apply Pointer _ m@(Machine{mem=(h:r), ..}) =
         return $ m {
                    dirPointer = circularMove dirPointer (fromInteger h),
                    mem=r
                    }
 
+apply Switch _ m@(Machine{mem=[], ..}) = return m
 apply Switch _ m@(Machine{mem=(h:r), ..}) =
         return $ m {
                    codelChooser = circularMove codelChooser (fromInteger h),
                    mem = r
         }
 
+apply Duplicate _ m@(Machine{mem=[], ..}) = return m
 apply Duplicate _ m = return . updMemWith (\(x:r) -> x:x:r) $ m
 
+apply Roll _ m@(Machine{mem=[], ..}) = return m
+apply Roll _ m@(Machine{mem=[_], ..}) = return m
 apply Roll _ m@(Machine {mem=(x:y:r), ..}) =
         return $ m {mem = roll (fromInteger y) (fromInteger x) r}
 
@@ -85,10 +94,12 @@ apply InChar _ m = do
     n <- toInteger . ord . head <$> getLine
     return . updMemWith (n:) $ m
 
+apply OutInt _ m@(Machine{mem=[_], ..}) = return m
 apply OutInt _ m@(Machine{mem=(h:r), ..}) = do
     print h
     return $ m{ mem = r}
 
+apply OutChar _ m@(Machine{mem=[_], ..}) = return m
 apply OutChar _ m@(Machine{mem=(h:r), ..}) = do
     putChar . chr . fromInteger $ h
     return $ m{ mem = r}
