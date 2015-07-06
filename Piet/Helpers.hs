1bca6c48514d63f1afb31d5fcc6c8e63dd791968
lesguillemets <lesguillemets@users.noreply.github.com>
2015-07-06 12:06:46 +0900

    circDiff

--- Piet/Helpers.hs
+++ Piet/Helpers.hs
@@ -30,7 +30,20 @@ circularMove :: (Bounded a, Enum a) => a -> Int -> a
 circularMove e n = let
     m = 1+fromEnum (maxBound `asTypeOf` e)
     in
-        toEnum . (`mod` m) $ n + fromEnum e + m
+        toEnum . (`rem` m) $ n + fromEnum e + m
+
+-- TODO : perhaps we'd better flip
+circDiff :: (Bounded a, Enum a) => a -> a -> Int
+circDiff x y = let
+    m = 1 + fromEnum (maxBound `asTypeOf` x)
+    d = (fromEnum y - fromEnum x) `rem` m
+    in
+       if d < 0 then d+m else d
+-- |
+-- >>> circDiff B E -- B -> C -> D -> E
+-- 3
+-- >>> circDiff D B -- D -> E -> A -> b
+-- 3
 
 data Direction = Up | Rt | Dw | Lf deriving (Bounded,Enum,Show)
 
