c9977a686cbc59682681b1c8a5523349410a0a07
lesguillemets <lesguillemets@users.noreply.github.com>
2015-07-07 15:42:20 +0900

    test for command

--- Piet/Tester.hs
+++ Piet/Tester.hs
@@ -1,4 +1,41 @@
 import Colours
 import Commands
-import Helpers
 import Machine
+import Data.List (foldl')
+import Data.Maybe
+import Control.Monad
+
+-- the trace of the sample program
+-- (
+--
+trace0 :: [(Colour,Integer)]
+trace0 = zip [
+             Chromatic Normal Red, Chromatic Dark Red,
+             Chromatic Normal Magenta, Chromatic Dark Magenta,
+             Chromatic Normal Blue, Chromatic Dark Blue,
+             Chromatic Dark Green, Chromatic Dark Red,
+             Chromatic Normal Magenta, Chromatic Light Blue,
+             Chromatic Normal Blue, Chromatic Normal Green,
+             Chromatic Light Yellow, Chromatic Normal Yellow,
+             Chromatic Light Red, Chromatic Normal Red,
+             Chromatic Light Magenta, Chromatic Dark Blue,
+             Chromatic Light Blue, Chromatic Dark Cyan,
+             Chromatic Normal Green, Chromatic Dark Green,
+             Chromatic Normal Yellow, Chromatic Dark Yellow,
+             Chromatic Normal Red ]
+         [
+         72,1,101,3,108,2,2,1,1,111,5,5,32,16,119,10,3,114,1,1,100,1,33,1,4
+         ]
+
+followTrace :: [(Colour,Integer)] -> IO ()
+followTrace t = do
+    let m = initialMachine
+        cmds = zipWith
+                (\(c0,x) (c1,_) -> (fromColours c0 c1,x)) t (tail t)
+    ml <- flip connect m $ map (\(c,w) -> apply (fromJust c) w) cmds
+    print ml
+
+connect :: Monad m => [a -> m a] -> a -> m a
+connect = foldl' (>=>) return
+
+main = followTrace trace0
