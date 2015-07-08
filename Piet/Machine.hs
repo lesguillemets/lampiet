4d1114ad6f70c5cbcd606c97545f20bc71a3f763
lesguillemets <lesguillemets@users.noreply.github.com>
2015-07-08 11:42:03 +0900

    add loc to machine

--- Piet/Machine.hs
+++ Piet/Machine.hs
@@ -7,8 +7,9 @@ type Mem = [Integer]
 data Machine = Machine {
              dirPointer :: Direction,
              codelChooser :: RL,
+             loc :: (Int,Int),
              mem :: [Integer]
 } deriving (Show)
 
 initialMachine :: Machine
-initialMachine = Machine Rt RLLeft []
+initialMachine = Machine Rt RLLeft (0,0) []
