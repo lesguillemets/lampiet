1274f6c6322357edb0034768cd94c456c451aa4b
lesguillemets <lesguillemets@users.noreply.github.com>
2015-07-12 14:07:27 +0900

    Direction/towards

--- Piet/Direction.hs
+++ Piet/Direction.hs
@@ -20,3 +20,17 @@ turnRight = flip circularMove 1
 -- |
 -- >>> map turnRight $ enumFrom Up
 -- [Rt,Dw,Lf,Up]
+
+-- FIXME :: saner inprementation
+towards :: Direction -> Loc -> Loc -> Bool
+towards Up (_,y0) (_,y) = y <= y0
+towards Dw (_,y0) (_,y) = y0 <= y
+towards Rt (x0,_) (x,_) = x0 <= x
+towards Lf (x0,_) (x,_) = x <= x0
+-- |
+-- towards direction loc0 loc1 :
+-- is <loc1> is on <direction> when looked from <loc0>?
+-- >>> towards Up (5,5) (10,2)
+-- True
+-- >>> towards Rt (2,3) (5,0)
+-- True
