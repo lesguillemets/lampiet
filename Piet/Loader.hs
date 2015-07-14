d75e7ff4dc769bfe206507400a6887278271a782
lesguillemets <lesguillemets@users.noreply.github.com>
2015-07-14 11:35:52 +0900

    basic structure for loadimg

--- Piet/Loader.hs
+++ Piet/Loader.hs
@@ -1,9 +1,28 @@
 module Loader where
 
+import Codec.Picture -- Juicypixel
 import qualified Data.Array.IArray as IA
+import qualified Data.Vector as V
+import Data.Word (Word8)
 
 import Codel
 import Colours
+import Helpers
 
 loadImg :: FilePath -> IO Loaded
 loadImg = undefined
+
+readImg :: FilePath -> IO (Either String (IA.Array Loc (Word8,Word8,Word8)))
+readImg fname = do
+    img <- readImage fname
+    case img of
+        (Left s) -> return $ Left s
+        (Right im) ->
+            case im of
+                (ImageRGB8 i) -> return $ Right (toArr i)
+                _ -> return $ Left "unsupported color space?"
+
+
+toArr :: Image PixelRGB8 -> IA.Array Loc (Word8,Word8,Word8)
+toArr = undefined
+
